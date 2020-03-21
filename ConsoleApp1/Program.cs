e23b565733c77a668ec60159358af907e18a800f
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-21 19:55:39 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static int width = 1200, height = 720;
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("Wirtualna Kamera");
+
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(onDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        private static void OnRenderFrame()
+        {
+
+            
+        }
+
+        private static void onDisplay()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
+        }
+    }
+}
