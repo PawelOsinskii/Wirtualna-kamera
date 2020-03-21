eb0497daea4e9471f7a8090454f661023f74bf78
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-03-21 23:36:22 +0100

    pokolorowanie figur

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
     class Program
     {
         private static int width = 1200, height = 720;
+        private static VBO<Vector3> triangleColor, squareColor;
 
         private static ShaderProgram program;
         private static VBO<Vector3> triangle, square;
@@ -34,6 +35,10 @@ namespace ConsoleApp1
 
             triangleElements = new VBO<int>(new int[] { 0, 1, 2 }, BufferTarget.ElementArrayBuffer);
             squareElements = new VBO<int>(new int[] { 0, 1, 2, 3 }, BufferTarget.ElementArrayBuffer);
+
+            triangleColor = new VBO<Vector3>(new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) });
+            squareColor = new VBO<Vector3>(new Vector3[] { new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1), new Vector3(0.5, 0.5, 1) });
+
             Glut.glutMainLoop();
         }
 
@@ -53,7 +58,7 @@ namespace ConsoleApp1
             Gl.BindBuffer(triangle);
             Gl.VertexAttribPointer(vertexPositionIndex, triangle.Size, triangle.PointerType, true, 12, IntPtr.Zero);
 
-           
+            Gl.BindBufferToShaderAttribute(triangleColor,program, "vertexColor");
 
 
 
@@ -63,6 +68,8 @@ namespace ConsoleApp1
             //draw square
             program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
             Gl.BindBufferToShaderAttribute(square, program, "vertexPosition");
+            
+            Gl.BindBufferToShaderAttribute(squareColor, program, "vertexColor");
             Gl.BindBuffer(squareElements);
             Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
 
@@ -77,19 +84,23 @@ namespace ConsoleApp1
 
         public static string VertexShader = @"
 in vec3 vertexPosition;
+in vec3 vertexColor;
+out vec3 color;
 
 uniform mat4 projection_matrix;
 uniform mat4 view_matrix;
 uniform mat4 model_matrix;
 
 void main(void){
+    color = vertexColor;
     gl_Position = projection_matrix * view_matrix * model_matrix *vec4(vertexPosition,1);
 }
 
 ";
         public static string FragmentShader = @"
+in vec3 color;
 void main(void){
-    gl_FragColor = vec4(1,1,1,1);
+    gl_FragColor = vec4(color,1);
 }
 ";
 
